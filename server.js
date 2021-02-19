fb35ce633f8ea29596c64fd34eb9373e7f913110
Adam Hayward <a.hayward11@gmail.com>
2021-02-18 22:14:48 -0900

    fixed file structure

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+// dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+// sets up Express App
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
-const app = express();
-
-app.use(logger("dev"));
-
+// app.use(logger("dev"));
+// Sets up Express to handle data parssing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// static directory
 app.use(express.static("public"));
-
+// connecting to mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,6 +26,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
 
+
+// starts Express app
 app.listen(PORT, () => {
   console.log(`App running on port: localhost:${PORT}!`);
 });
